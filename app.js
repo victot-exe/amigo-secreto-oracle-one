2575b39b7829412bed12b30e4c4e872e9e170144
Victot <victorfarian@gmail.com>
2025-01-20 20:39:34 -0300

    initializing repo

---
+++ app.js
@@ -0,0 +1,8 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+function adicionarAmigo(){
+    alert("Botao clicado")
+}
+
+function sortearAmigo(){
+    alert("Botao clicado2")
+}
