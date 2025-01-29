89e719d3147d6d301c9c98da84c3a231aad0c324
Victot <victorfarian@gmail.com>
2025-01-28 23:12:37 -0300

    add function gerarNumeroAleatorio()

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaDeAmigos = [];
-
+let numerosGerados = [];
 
 function adicionarAmigo(){
     let inputName = document.getElementById("amigo");
@@ -13,4 +13,27 @@ function adicionarAmigo(){
         inputName.value = "";
         alert(`${nome} foi adicionado a lista.`);
     }
+}
+
+function sortearAmigo(){
+    //bloquear e limpar a barra de digitar
+    //Gerarnumero aleatorio
+    //se for -1 bloquear botao de sortear e dizer que os nomes foram sorteados
+    //mostrar nome sorteado referente
+}
+
+function gerarNumeroAleatorio(){
+
+    if(numerosGerados.length === listaDeAmigos.length){
+        return -1;
+    }
+
+    let numeroGerado;
+
+    do{
+        numeroGerado = Math.floor(Math.random() * listaDeAmigos.length);
+    }while(numerosGerados.includes(numeroGerado))
+    
+    numerosGerados.push(numeroGerado);
+    return numeroGerado;
 }
