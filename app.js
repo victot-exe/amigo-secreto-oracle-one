f2de4853688321c931afdc1146d00e2ca358c5db
Victot <victorfarian@gmail.com>
2025-01-28 23:11:47 -0300

    add function adicionarAmigo()

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-function adicionarAmigo(){
-    alert("Botao clicado")
-}
+let listaDeAmigos = [];
 
-function sortearAmigo(){
-    alert("Botao clicado2")
+
+function adicionarAmigo(){
+    let inputName = document.getElementById("amigo");
+    let nome = inputName.value;
+    
+    if(listaDeAmigos.some(n => n.toLowerCase() === nome.toLowerCase())){
+        alert(`O nome ${nome} já consta na lista, por favor coloque outro.`);
+    }else{
+        listaDeAmigos.push(nome);
+        inputName.value = "";
+        alert(`${nome} foi adicionado a lista.`);
+    }
 }
