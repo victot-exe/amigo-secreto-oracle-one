6c8daaafab4396f6bffd5819b0ded73460a8272e
Victot <victorfarian@gmail.com>
2025-02-12 19:53:13 -0300

    melhorando algumas verificações de nome e algumas funcões adicionadas

--- app.js
+++ app.js
@@ -1,39 +1,87 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let listaDeAmigos = [];
-let numerosGerados = [];
+const sorteio ={
+    listaDeAmigos: [],
+    numerosGerados: [],
+}
 
 function adicionarAmigo(){
     let inputName = document.getElementById("amigo");
     let nome = inputName.value;
     
-    if(listaDeAmigos.some(n => n.toLowerCase() === nome.toLowerCase())){
+    if(!nome.trim()){
+        alert("por favor insira um nome válido");
+        return;
+    }
+
+    if(sorteio.listaDeAmigos.some(n => n.trim().toLowerCase() === nome.trim().toLowerCase())){
         alert(`O nome ${nome} já consta na lista, por favor coloque outro.`);
     }else{
-        listaDeAmigos.push(nome);
+        sorteio.listaDeAmigos.push(nome);
         inputName.value = "";
         alert(`${nome} foi adicionado a lista.`);
     }
 }
 
 function sortearAmigo(){
-    //bloquear e limpar a barra de digitar
-    //Gerarnumero aleatorio
-    //se for -1 bloquear botao de sortear e dizer que os nomes foram sorteados
-    //mostrar nome sorteado referente
+
+    if(sorteio.listaDeAmigos.length < 3){
+        alert("Você precisa de pelo menos três amiguinhos para iniciar o sorteio :)")
+        return;
+    }
+
+    if(sorteio.numerosGerados.length === 0){
+        desativarInput();
+        desativarAdicionar();
+    }
+    
+    let numeroGerado = gerarNumeroAleatorio();
+    
+    if(numeroGerado < 0){
+        alert("todos os nomes já foram sorteados");
+        reiniciarOJogo();
+        return;
+    }else{
+        alert(`Você tirou o/a ${sorteio.listaDeAmigos[numeroGerado]}`)
+    }
+    
 }
 
 function gerarNumeroAleatorio(){
 
-    if(numerosGerados.length === listaDeAmigos.length){
+    if(sorteio.numerosGerados.length === sorteio.listaDeAmigos.length){
         return -1;
     }
 
     let numeroGerado;
 
     do{
-        numeroGerado = Math.floor(Math.random() * listaDeAmigos.length);
-    }while(numerosGerados.includes(numeroGerado))
+        numeroGerado = Math.floor(Math.random() * sorteio.listaDeAmigos.length);
+    }while(sorteio.numerosGerados.includes(numeroGerado))
     
-    numerosGerados.push(numeroGerado);
+    sorteio.numerosGerados.push(numeroGerado);
     return numeroGerado;
+}
+
+function desativarInput(){
+    let inputName = document.getElementById("amigo");
+    inputName.value = "";
+    inputName.disabled = true;
+    inputName.placeholder = "Os nomes já foram definidos";
+}
+
+function desativarAdicionar(){
+    document.getElementById("adicionar").disabled = true;
+}
+
+function reiniciarOJogo(){
+    alert("Todos os nomes foram sorteados, reiniciando o jogo...");
+
+    sorteio.listaDeAmigos = [];
+    sorteio.numerosGerados = [];
+
+    let inputName = document.getElementById("amigo");
+    inputName.disabled = false;
+    inputName.placeholder = "Digite um nome";
+
+    document.getElementById("adicionar").disabled = false;
 }
